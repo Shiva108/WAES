387ed30cc45e190089e9db4b16d26bf08cb5c3e5
Shiva108 <shiva108@protonmail.com>
2019-07-27 13:46:11 +0200

    update to supergo

--- supergobuster.sh
+++ supergobuster.sh
@@ -3,17 +3,17 @@ set -eu
 
 URL=$1
 
-echo "super go bustering for super brute: $URL"
+echo "super go bustering for super brute: -u $URL"
 
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/tomcat.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/nginx.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/apache.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/RobotsDisallowed-Top1000.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/ApacheTomcat.fuzz.txt
-# gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web_Content/sharepoint.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w SecLists/Discovery/Web-Content/iis.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x txt
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x php
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x doc
-gobuster -u $URL -l -s 200,204,301,302,307,403 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x docx
+gobuster  dir -u $URL -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/tomcat.txt --wildcard
+gobuster  dir -u $URL -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/nginx.txt --wildcard
+gobuster  dir -u $URL -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/apache.txt --wildcard
+gobuster  dir -u $URL -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/RobotsDisallowed-Top1000.txt --wildcard
+gobuster  dir -u $URL  -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/ApacheTomcat.fuzz.txt --wildcard
+# gobuster  dir -u $URL  -w /home/e/CTF-notes/SecLists/Discovery/Web_Content/sharepoint.txt --wildcard
+gobuster  dir -u $URL  -w /home/e/CTF-notes/SecLists/Discovery/Web-Content/iis.txt --wildcard
+gobuster  dir -u $URL  -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt --wildcard
+gobuster  dir -u $URL  -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x txt  --wildcard
+gobuster  dir -u $URL  -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x php --wildcard
+gobuster  dir -u $URL  -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x doc --wildcard
+gobuster  dir -u $URL  -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -e -x docx --wildcard
