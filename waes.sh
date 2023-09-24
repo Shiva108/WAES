63e84e6570cd4cc2d9dd73063bafd328e5d9ffff
sh1v4x6c <shiva108@protonmail.com>
2023-09-24 14:27:55 +0200

    updates

--- waes.sh
+++ waes.sh
@@ -11,7 +11,7 @@
 # set -x # Starts debugging
 
 # vars
-VERSION="0.0.36 alpha"
+VERSION="0.0.37 alpha"
 VULNERSDIR="vulscan" # Where to find vulscan
 REPORTDIR="report" # /report directory
 TOOLS=( "nmap" "nikto" "uniscan" "gobuster" "dirb" "whatweb" "wafw00f" )
@@ -43,10 +43,10 @@ echo "       Example: ./waes.sh -u 10.10.10.130 -p 8080"
 echo ""
 }
 
-if [[ `id -u` -ne 0 ]] ; then echo -e "\e[01;31m[!]\e[00m This program must be run as root. Run again with 'sudo'" ; exit 1 ; fi
+if [[ $(id -u) -ne 0 ]] ; then echo -e "\e[01;31m[!]\e[00m This program must be run as root. Run again with 'sudo'" ; exit 1 ; fi
 
 # Checks for input parameters
-: ${1?"No arguments supplied - run waes -h for help or cat README.md"}
+: "${1?"No arguments supplied - run waes -h for help or cat README.md"}"
 
 # Showing parameters - for debugging only
 #echo "Positional Parameters"
@@ -78,7 +78,7 @@ fi
 while [[ "x${TOOLS[COUNT]}" != "x" ]]
 do
    COUNT=$(( $COUNT + 1 ))
-   if ! hash ${TOOLS[COUNT]} /dev/null 2>&1
+   if ! hash "${TOOLS[COUNT]}" /dev/null 2>&1
     then
         echo -e "\e[01;31m[!]\e[00m ${TOOLS[COUNT]} was not found in PATH"
         echo "Run sudo ./install.sh to install tools"
@@ -94,23 +94,23 @@ passive() {
 
     echo "Starting PASSIVE scans..."
     # Whatweb
-    echo -e "\e[00;32m [+] Looking up "$2" with whatweb - only works for online targets" "\e[00m"
-    whatweb -a 3 $2":"$PORT | tee ${REPORTDIR}/$2_whatweb.txt
+    echo -e "\e[00;32m [+] Looking up ""$2"" with whatweb - only works for online targets" "\e[00m"
+    whatweb -a 3 "$2"":""$PORT" | tee ${REPORTDIR}/"$2"_whatweb.txt
 
     # OSIRA - For subdomain enum
-    echo -e "\e[00;32m [+] OSIRA against:" $2 " - looking for subdomains \e[00m"
-    OSIRA/osira.sh -u $2":"$PORT | tee ${REPORTDIR}/$2_osira.txt
+    echo -e "\e[00;32m [+] OSIRA against:" "$2" " - looking for subdomains \e[00m"
+    OSIRA/osira.sh -u "$2"":""$PORT" | tee ${REPORTDIR}/"$2"_osira.txt
 }
 
 fastscan() {
 
     echo "Step 1: Starting fast scan... "
     # wafw00f
-    echo -e "\e[00;32m [+] Detecting firewall "$2":"$PORT" with wafw00f" "\e[00m"
-    wafw00f -a -v $2":"$PORT | tee $REPORTDIR/$2_wafw00f.txt
+    echo -e "\e[00;32m [+] Detecting firewall ""$2"":""$PORT"" with wafw00f" "\e[00m"
+    wafw00f -a -v "$2"":""$PORT" | tee $REPORTDIR/"$2"_wafw00f.txt
     # nmap http-enum
     echo -e "\e[00;32m [+] nmap with HTTP-ENUM script against $2" "\e[00m"
-    nmap -sSV -Pn -T4 -p $PORT --script http-enum $2 -oA ${REPORTDIR}/$2_nmap_http-enum
+    nmap -sSV -Pn -T4 -p "$PORT" --script http-enum "$2" -oA ${REPORTDIR}/"$2"_nmap_http-enum
 }
 
 scan() {
@@ -118,17 +118,17 @@ scan() {
     echo "Step 2: Starting more in-depth scan... "
     # nmap
     echo -e "\e[00;32m [+] nmap with various HTTP scripts against $2" "\e[00m"
-    nmap -sSV -Pn -T4 -p $PORT --script $HTTPNSE $2 -oA ${REPORTDIR}/$2_nmap_http-va
+    nmap -sSV -Pn -T4 -p "$PORT" --script $HTTPNSE "$2" -oA ${REPORTDIR}/"$2"_nmap_http-va
     echo -e "\e[00;32m [+] nmap with vulscan on $2 with min CVSS 5.0" "\e[00m"
-    nmap -sSV -Pn -O -T4 --version-all -p $PORT --script ${VULNERSDIR}/vulscan.nse $2 --script-args mincvss=5-0 -oA ${REPORTDIR}/$2_nmap_vulners
+    nmap -sSV -Pn -O -T4 --version-all -p "$PORT" --script ${VULNERSDIR}/vulscan.nse $2 --script-args mincvss=5-0 -oA ${REPORTDIR}/$2_nmap_vulners
 
     # nikto
     echo -e "\e[00;32m [+] nikto on $2" "\e[00m"
-    nikto -h $2 -port $PORT -C all -ask no -evasion A | tee $REPORTDIR/$2_nikto.txt
+    nikto -h "$2" -port "$PORT" -C all -ask no -evasion A | tee $REPORTDIR/"$2"_nikto.txt
 
     # uniscan
     echo -e "\e[00;32m [+] uniscan of $2" "\e[00m"
-    uniscan -u $2":"$PORT -qweds | tee $REPORTDIR/$2_uniscan.txt
+    uniscan -u "$2"":""$PORT" -qweds | tee $REPORTDIR/"$2"_uniscan.txt
 }
 
 fuzzing() {
@@ -140,7 +140,7 @@ fuzzing() {
 
     # Supergobuster: gobuster + dirb
     echo -e "\e[00;32m [+] super go busting $2" "\e[00m"
-    ./supergobuster.sh "http://"$2":"$PORT | tee $REPORTDIR/$2_supergobust.txt
+    ./supergobuster.sh "http://""$2"":""$PORT" | tee $REPORTDIR/"$2"_supergobust.txt
 }
 
 end() {
@@ -148,10 +148,10 @@ end() {
 }
 
 # passive  $1 $2 $3 $4 # Uncomment to run, work online for online targets Todo: Add in next version
-fastscan $1 $2 $3 $4
-scan $1 $2 $3 $4
-fuzzing  $1 $2 $3 $4
-end $1 $2 $3 $4
+fastscan "$1" "$2" "$3" "$4"
+scan "$1" "$2" "$3" "$4"
+fuzzing  "$1" "$2" "$3" "$4"
+end "$1" "$2" "$3" "$4"
 
 # Todo: Add from rapidscan / golismero and others
 
@@ -159,8 +159,8 @@ end $1 $2 $3 $4
 echo -e "Target: $2 "
 
 # Whatweb
-echo -e "\e[00;32m [+] Looking up "$2" with whatweb" "\e[00m"
-whatweb -a3 $2 | tee ${REPORTDIR}/$2_whatweb.txt
+echo -e "\e[00;32m [+] Looking up ""$2"" with whatweb" "\e[00m"
+whatweb -a3 "$2" | tee ${REPORTDIR}/"$2"_whatweb.txt
 
 # echo -e "\e[00;32m [+] OSIRA against:" $2 "\e[00m"
 # OSIRA/osira.sh -u $2 | tee ${REPORTDIR}/$2_osira.txt
@@ -168,9 +168,9 @@ whatweb -a3 $2 | tee ${REPORTDIR}/$2_whatweb.txt
 
 # nmap
 echo -e "\e[00;32m [+] nmap with standard scripts (-sC) on $2" "\e[00m"
-nmap -sSCV -Pn -T4 $2 -oA ${REPORTDIR}/$2_nmap_sSCV
+nmap -sSCV -Pn -T4 "$2" -oA ${REPORTDIR}/"$2"_nmap_sSCV
 echo -e "\e[00;32m [+] nmap with http-enum against $2" "\e[00m"
-nmap -sSV -Pn -T4 --script http-enum $2 -oA ${REPORTDIR}/$2_nmap_http-enum
+nmap -sSV -Pn -T4 --script http-enum "$2" -oA ${REPORTDIR}/"$2"_nmap_http-enum
 # echo -e "\e[00;32m [+] nmap with various HTTP scripts against $2" "\e[00m"
 # nmap -sSV -Pn -T4 --script "http-*" $2 -oA ${REPORTDIR}/$2_nmap_http-va
 # echo -e "\e[00;32m [+] nmap with vulners on $2" "\e[00m"
@@ -179,15 +179,15 @@ nmap -sSV -Pn -T4 --script http-enum $2 -oA ${REPORTDIR}/$2_nmap_http-enum
 
 # nikto
 echo -e "\e[00;32m [+] nikto on $2" "\e[00m"
-nikto -h $2 -C all -ask no -evasion A | tee $REPORTDIR/$2_nikto.txt
+nikto -h "$2" -C all -ask no -evasion A | tee $REPORTDIR/"$2"_nikto.txt
 
 # uniscan
 echo -e "\e[00;32m [+] uniscan of $2" "\e[00m"
-uniscan -u $2 -qweds | tee $REPORTDIR/$2_uniscan.txt
+uniscan -u "$2" -qweds | tee $REPORTDIR/"$2"_uniscan.txt
 
 # Supergobuster: gobuster + dirb
 echo -e "\e[00;32m [+] super go busting $2" "\e[00m"
-./supergobuster.sh $2 | tee $REPORTDIR/$2_supergobust.txt
+./supergobuster.sh "$2" | tee $REPORTDIR/"$2"_supergobust.txt
 
 echo -e "\e[00;32m [+] WAES is done. Find results in:" ${REPORTDIR} "\e[00m"
 # set +x # Ends debugging
